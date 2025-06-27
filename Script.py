0e410d89628a8c5ed34e7e8335e141967799b522
Rajesh Kumar naik <naik57149@gmail.com>
2025-06-27 10:27:02 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -241,7 +241,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b>{file_name}\nUploaded By: <a herf="https://t.me/SilentXBotz">[SilentXBotz]</a></b>"""
+    CAPTION = """<b>{file_name}\nUploaded By: <a herf="https://t.me/KRxFILM">[ KR x FILM ]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
